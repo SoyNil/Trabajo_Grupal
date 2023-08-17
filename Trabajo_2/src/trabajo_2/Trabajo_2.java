8cd21e8d93d6c6410aedefda38202596b4349291
SoyNil <1422624@senati.pe>
2023-08-17 14:52:45 -0500

    Condicionales resta y división

--- Trabajo_2/src/trabajo_2/Trabajo_2.java
+++ Trabajo_2/src/trabajo_2/Trabajo_2.java
@@ -11,5 +11,14 @@ public class Trabajo_2 {
         n1=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer número"));
         n2=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número")); 
         int op=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número\n1.- Suma\n2.- Resta\n3.- Multiplicación\n4.- División"));
+        //Condicionales
+        if (op==2){
+            resultado=n1-n2;
+            JOptionPane.showMessageDialog(null,"Los números que ingresaste son "+n1+" y "+n2+", la resta de estos es "+ resultado);
+        }
+        if (op==4){
+            resultado=n1/n2;
+            JOptionPane.showMessageDialog(null,"Los números que ingresaste son "+n1+" y "+n2+", la división de estos es "+resultado);
+        }
 }
 }
