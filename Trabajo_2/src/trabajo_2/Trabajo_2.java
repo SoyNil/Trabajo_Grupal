489a94c1724e175b4fb066cedbdcf8f2d6f0f90e
SoyNil <1422624@senati.pe>
2023-08-17 14:39:29 -0500

    Panel principal

--- Trabajo_2/src/trabajo_2/Trabajo_2.java
+++ Trabajo_2/src/trabajo_2/Trabajo_2.java
@@ -4,12 +4,12 @@ import javax.swing.JOptionPane;
 
 public class Trabajo_2 {
     public static void main(String[] args) {
-        //Variables
+        //Panel Principal
         int n1;
         int n2;
-        //Resultado
         int resultado;
         n1=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer número"));
         n2=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número")); 
+        int op=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número\n1.- Suma\n2.- Resta\n3.- Multiplicación\n4.- División"));
 }
 }
