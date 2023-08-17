7f9693da5aed4e9d55981b110b6b82742a83d2a1
SoyNil <1422624@senati.pe>
2023-08-17 14:31:14 -0500

    Java

---
+++ Trabajo_2/src/trabajo_2/Trabajo_2.java
@@ -0,0 +1,15 @@
+package trabajo_2;
+//Import JoptionPane
+import javax.swing.JOptionPane;
+
+public class Trabajo_2 {
+    public static void main(String[] args) {
+        //Variables
+        int n1;
+        int n2;
+        //Resultado
+        int resultado;
+        n1=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer número"));
+        n2=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número")); 
+}
+}
